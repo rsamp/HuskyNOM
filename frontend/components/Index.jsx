f0d7a0e339a48874bcb5837b5bd0ef75adaf6ab0
Ryan Samp <sampryan@gmail.com>
2015-12-15 14:21:58 -0800

    Map rendering

--- frontend/components/Index.jsx
+++ frontend/components/Index.jsx
@@ -1,13 +1,18 @@
 var React = require('react'),
-    BusinessIndex = require('./business/Index');
+    BusinessIndex = require('./business/Index'),
+    BusinessStore = require('../stores/business'),
+    Map = require('./Map');
 
 var Index = React.createClass({
-
+  getInitialState: function(){
+    return {businesses: BusinessStore.all()}
+  },
 
   render: function(){
     return(
       <div className="searchResults">
         <BusinessIndex/>
+        <Map businesses={this.state.businesses}/>
       </div>
     );
   }
---
+++ frontend/components/Map.jsx
@@ -0,0 +1,92 @@
+var React = require('react'),
+    ReactDOM = require('react-dom');
+
+function _getCoordsObj(latLng) {
+  return {
+    lat: latLng.lat(),
+    lng: latLng.lng()
+  };
+}
+
+var Map = React.createClass({
+  componentDidMount: function(){
+    console.log('map mounted');
+    var map = ReactDOM.findDOMNode(this.refs.map);
+    var mapOptions = {
+      center: {lat: 47.655, lng: -122.308},
+      zoom: 13
+    };
+    this.map = new google.maps.Map(map, mapOptions);
+    this.registerListeners();
+    this.markers = [];
+    this.props.businesses.forEach(this.createMarker);
+  },
+
+  registerListeners: function(){
+    google.maps.event.addListener(this.map, 'idle', function(){
+      var bounds = this.map.getBounds();
+      var northEast = _getCoordsObj(bounds.getNorthEast());
+      var southWest = _getCoordsObj(bounds.getSouthWest());
+      var bounds = {northEast: northEast, southWest: southWest};
+    }.bind(this));
+  },
+
+  createMarker: function(business){
+    var pos = new google.maps.LatLng(business.lat, business.lng)
+    var marker = new google.maps.Marker({
+      position: pos,
+      map: this.map,
+      title: business.name,
+      businessId: business.id
+    });
+    this.markers.push(marker);
+  },
+
+  removeMarker: function(marker){
+    for(var i = 0; i < this.markers.length; i++){
+      if (this.markers[i].businessId === marker.businessId){
+        this.markers[i].setMap(null);
+        this.markers.splice(i, 1);
+        break;
+      }
+    }
+  },
+
+  componentDidUpdate: function(oldProps){
+    this._onChange();
+  },
+
+  _onChange: function(){
+    var businesses = this.props.businesses;
+    var toAdd = [];
+    var toRemove = this.markers.slice(0);
+    businesses.forEach(function(business, idx){
+      var idx = -1;
+
+      for(var i = 0; i < toRemove.length; i ++){
+        if(toRemove[i].businessId == business.id){
+          idx = i;
+          break;
+        }
+      }
+      
+      if (idx === -1) {
+        toAdd.push(bench);
+      } else {
+        toRemove.splice(idx, 1);
+      }
+    });
+    toAdd.forEach(this.createMarker);
+    toRemove.forEach(this.removeMarker);
+  },
+
+  componentWillUnmount: function(){
+    console.log("map unmounted");
+  },
+
+  render: function(){
+    return (<div ref="map" className="indexMap">Map</div>)
+  }
+});
+
+module.exports = Map;
